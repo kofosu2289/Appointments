cae6eb7c606e4f76c7c99c8c9dfa675531788bb9
Kenneth Ofosu <kofosu89@gmail.com>
2019-11-29 07:34:24 -0500

    pass test to check if the customer form renders a text box for the first name

--- src/CustomerForm.js
+++ src/CustomerForm.js
@@ -1,3 +1,11 @@
+/* eslint-disable import/prefer-default-export */
 import React from 'react';
 
-export const CustomerForm = () => <form id="customer" />;
+export const CustomerForm = () => (
+  <form id="customer">
+    <input
+      type="text"
+      name="firstName"
+    />
+  </form>
+);
