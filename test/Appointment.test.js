06116153d6e90a5c97e4579d92c981dde1afc88f
Kenneth Ofosu <kofosu89@gmail.com>
2019-11-29 05:18:13 -0500

    define and skip test to check if appointment div renders a new appt when it is clicked

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
 
 import {
   Appointment,
@@ -97,5 +98,12 @@ describe('AppointmentsDayView', () => {
     expect(
       container.querySelectorAll('li > button')[0].type
     ).toEqual('button');
+  });
+
+  it.skip('renders another appointment when selected', () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    const button = container.querySelectorAll('button')[1];
+    ReactTestUtils.Simulate.click(button);
+    expect(container.textContent).toMatch('Jordan');
   });
 });
