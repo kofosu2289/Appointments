9759250567eb3c2aa75a2563244d5c8e7713db84
Kenneth Ofosu <kofosu89@gmail.com>
2019-12-01 14:16:54 -0500

    define test to check that form will already contain an existing first name during modification

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -30,4 +30,10 @@ describe('CustomerForm', () => {
     const field = form('customer').elements.firstName;
     expectToBeInputFieldOfTypeText(field);
   });
+
+  it('includes the existing value for the first name', () => {
+    render(<CustomerForm firstName="Ashley" />);
+    const field = form('customer').elements.firstName;
+    expect(field.value).toEqual('Ashley');
+  })
 });
