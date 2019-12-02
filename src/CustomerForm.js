55a7beba3fa053ed698e12e6ae72072f038e6fd7
Kenneth Ofosu <kofosu89@gmail.com>
2019-12-02 14:02:28 -0500

    pass test to check if a submitted first name is saved

--- src/CustomerForm.js
+++ src/CustomerForm.js
@@ -1,15 +1,18 @@
 /* eslint-disable import/prefer-default-export */
 import React from 'react';
 
-export const CustomerForm = ({ firstName }) => (
-  <form id="customer">
-    <label htmlFor="firstName">First name</label>
-    <input
-      type="text"
-      name="firstName"
-      id="firstName"
-      value={firstName}
-      readOnly
-    />
-  </form>
-);
+export const CustomerForm = ({ firstName, onSubmit }) => {
+  const customer = { firstName };
+  return (
+    <form id="customer" onSubmit={() => onSubmit(customer)}>
+      <label htmlFor="firstName">First name</label>
+      <input
+        type="text"
+        name="firstName"
+        id="firstName"
+        value={firstName}
+        readOnly
+      />
+    </form>
+  )
+};
