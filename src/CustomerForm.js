960c0322e7b068f5b0d70c162537e0c9a6886540
Kenneth Ofosu <kofosu89@gmail.com>
2019-12-02 13:44:47 -0500

    pass test to check if the first name field has a label

--- src/CustomerForm.js
+++ src/CustomerForm.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 export const CustomerForm = ({ firstName }) => (
   <form id="customer">
+    <label htmlFor="firstName">First name</label>
     <input
       type="text"
       name="firstName"
